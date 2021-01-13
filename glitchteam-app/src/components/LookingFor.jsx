ec443d6cc0f626e33ae4675449ee73a5d0e06da9
SalemDrenay <salemdrenay@gmail.com>
2021-01-13 22:08:18 +0100

    home page css done, waiting for validation

--- glitchteam-app/src/components/LookingFor.jsx
+++ glitchteam-app/src/components/LookingFor.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
+import Select from 'react-select';
+import { Link } from 'react-router-dom';
 import '../assets/css/styles.css';
 
 function LookingFor() {const options = [
-        { value: 'chocolate', label: 'Chocolate' },
-        { value: 'strawberry', label: 'Strawberry' },
-        { value: 'vanilla', label: 'Vanilla' }
+        { value: 'animaux', label: 'Animaux' },
+        { value: 'artsplastiques', label: 'Arts plastiques' },
+        { value: 'bienetre', label: 'Bien-être' },
+        { value: 'bricolage', label: 'Bricolage' },
+        { value: 'cuisine', label: 'Cuisine' },
+        { value: 'informatique', label: 'Informatique' },
+        { value: 'jardinage', label: 'Jardinage' },
+        { value: 'maison', label: 'Maison' },
+        { value: 'mecanique', label: 'Mécanique' },
+        { value: 'musique', label: 'Musique' },
+        { value: 'soutien', label: 'Soutien scolaire' }
     ]
 
     return (
-        <div>
-            <Select options={options} />
-        </div>    
+        <div className="home-looking">
+            <h2>Je recherche</h2>
+            <form className="home-form">
+                <Select options={options} placeholder="Spécialités" className="home-form-select" />
+                <input type="submit" value="Envoyer" className="home-page-validate" />
+            </form>
+            <div className="home-filters">
+                <Link to={`/home`} className="filter-card filter-art">
+                    <h3>Arts plastiques</h3>
+                </Link>
+                <Link to={`/home`} className="filter-card filter-cuisine">
+                    <h3>Cuisine</h3>
+                </Link><Link to={`/home`} className="filter-card filter-animaux">
+                    <h3>Animaux</h3>
+                </Link>
+                <Link to={`/home`} className="filter-card filter-bricolage">
+                    <h3>Bricolage</h3>
+                </Link>
+            </div>
+        </div>
     )
 }
 
--- glitchteam-app/src/components/Transmit.jsx
+++ glitchteam-app/src/components/Transmit.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
+import Select from 'react-select';
+import { Link } from 'react-router-dom';
 import '../assets/css/styles.css';
 
-function Transmit() {
+function Transmit() {const options = [
+        { value: 'animaux', label: 'Animaux' },
+        { value: 'artsplastiques', label: 'Arts plastiques' },
+        { value: 'bienetre', label: 'Bien-être' },
+        { value: 'bricolage', label: 'Bricolage' },
+        { value: 'cuisine', label: 'Cuisine' },
+        { value: 'informatique', label: 'Informatique' },
+        { value: 'jardinage', label: 'Jardinage' },
+        { value: 'maison', label: 'Maison' },
+        { value: 'mecanique', label: 'Mécanique' },
+        { value: 'musique', label: 'Musique' },
+        { value: 'soutien', label: 'Soutien scolaire' }
+    ]
+
     return (
-        <div>
-            <p>transmission</p>
+        <div className="home-transmit">
+            <h2>Je transmets</h2>
+            <form className="home-form">
+                <Select options={options} placeholder="Spécialités" className="home-form-select" />
+                <input type="submit" value="Envoyer" className="home-page-validate" />
+            </form>
+            <div className="home-filters">
+                <Link to={`/home`} className="filter-card filter-mecanique">
+                    <h3>Mécanique</h3>
+                </Link>
+                <Link to={`/home`} className="filter-card filter-musique">
+                    <h3>Musique</h3>
+                </Link><Link to={`/home`} className="filter-card filter-maison">
+                    <h3>Maison</h3>
+                </Link>
+                <Link to={`/home`} className="filter-card filter-animaux">
+                    <h3>Animaux</h3>
+                </Link>
+            </div>
         </div>    
     )
 }
