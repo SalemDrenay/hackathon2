27c4b1f3a035674b3500ec322a8ff6fb885ba6b5
GregJuge <gregoryjuge5@gmail.com>
2021-01-13 22:20:20 +0100

    inscription-user-password

--- glitchteam-app/src/components/LoginPage.jsx
+++ glitchteam-app/src/components/LoginPage.jsx
@@ -1,12 +1,37 @@
-import React from 'react';
-import '../assets/css/styles.css';
+// import react from "react";
+import "../assets/css/LoginPage.css";
 
 function LoginPage() {
-    return (
-        <div>
-            <p>Login</p>
-        </div>    
-    )
+  return (
+      
+    
+    <form>
+        {/* <div className="LogInPage"/> */}
+        <div className="NoAccount">
+        <p>Vous n&apos;avez pas de compte ?</p>
+       <button type="button">Créer son profil</button>
+
+        </div>
+        
+      <label className="IdArea">
+        Identifiant :
+        <input type="text" name="name" />
+      </label>
+      <label className="PasswordArea">
+        Mot de passe :
+        <input type="text" name="name" />
+      </label>
+      
+      <a className="IdButton">
+        <input
+        className="IdButton"
+          href="http://localhost:3000/home"
+          type="button"
+          value="Connexion"
+        />
+      </a>
+    </form>
+  );
 }
 
 export default LoginPage;
