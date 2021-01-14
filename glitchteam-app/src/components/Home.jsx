3b8c74d9a21d71196b82ef970db21f2f8cafbb2e
LoicLabaisse <69313495+LoicLabaisse@users.noreply.github.com>
2021-01-14 17:32:22 +0100

    add css

--- glitchteam-app/src/components/Home.jsx
+++ glitchteam-app/src/components/Home.jsx
@@ -10,12 +10,6 @@ function Home() {
     return (
         <div className="home-page">
             <div className="home-topbar">
-                <a href="#lookingfor" className="home-topbar-button home-topbar-looking">
-                    <p>Recherche</p>
-                </a>
-                <a href="#transmit" className="home-topbar-button home-topbar-transmit">
-                    <p>Proposition</p>
-                </a>
                 <div className="burger-menu">
                     <img id="burger-up" src={burger} alt="icon-menu-burger" />
                     <img id="burger-down" src={burger} alt="icon-menu-burger" />
@@ -28,6 +22,14 @@ function Home() {
                     <li> <a class="li-navMobile" href="#disconnect"> Déconnexion </a> </li>
                  </ul>
                 </div>
+            </div>
+            <div className="home-Search">
+            <a href="#lookingfor" className="home-topbar-button home-topbar-looking">
+                    <p>Recherche</p>
+                </a>
+                <a href="#transmit" className="home-topbar-button home-topbar-transmit">
+                    <p>Proposition</p>
+                </a>
             </div>
             <div className="home-container home-container-looking" id="lookingfor">
                 <LookingFor />
--- glitchteam-app/src/components/LoginPage.jsx
+++ glitchteam-app/src/components/LoginPage.jsx
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import "../assets/css/LoginPage.css";
 import logo from '../assets/images/logo.png'
 
